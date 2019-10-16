95173ac708135dd81b4a4e7f973e7205de56af45
norman-chen <nochen@xogrp.com>
2019-10-16 18:51:04 +0800

    etcd demo

---
+++ prepare/etcd_usage/demo1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+)
+
+func main() {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		putResp *clientv3.PutResponse
+		getResp *clientv3.GetResponse
+	)
+
+	config = clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"}, // 集群列表
+		DialTimeout: 5 * time.Second,
+	}
+
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	kv = clientv3.NewKV(client)
+
+	if putResp, err = kv.Put(context.TODO(), "/cron/jobs/job1", "hello1", clientv3.WithPrevKV()); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Revision: ", putResp.Header.Revision)
+		if putResp.PrevKv != nil {
+			fmt.Println("PrevVal: ", string(putResp.PrevKv.Value))
+		}
+	}
+
+	if getResp, err = kv.Get(context.TODO(), "/cron/jobs/job1"); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(getResp.Kvs, getResp.Count)
+	}
+
+	kv.Put(context.TODO(), "/cron/jobs/job2", "{...}")
+
+	if getResp, err = kv.Get(context.TODO(), "/cron/jobs", clientv3.WithPrefix()); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(getResp.Kvs, getResp.Count)
+	}
+	// fmt.Println(client)
+}
---
+++ prepare/etcd_usage/demo2/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.etcd.io/etcd/mvcc/mvccpb"
+
+	"go.etcd.io/etcd/clientv3"
+)
+
+func main() {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		delResp *clientv3.DeleteResponse
+		// getResp *clientv3.GetResponse
+		kvPair *mvccpb.KeyValue
+	)
+
+	config = clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"}, // 集群列表
+		DialTimeout: 5 * time.Second,
+	}
+
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	kv = clientv3.NewKV(client)
+
+	if delResp, err = kv.Delete(context.TODO(), "/cron/jobs/job1", clientv3.WithFromKey(), clientv3.WithLimit(2)); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if len(delResp.PrevKvs) != 0 {
+		for _, kvPair = range delResp.PrevKvs {
+			fmt.Println("deleted: ", string(kvPair.Key), string(kvPair.Value))
+		}
+	}
+}
