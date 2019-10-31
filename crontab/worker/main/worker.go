b57f587bf84ebcd12516aea9a804d9a0a229b814
norman-chen <nochen@xogrp.com>
2019-10-31 16:52:40 +0800

    stash

---
+++ crontab/worker/main/worker.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-crontab/crontab/worker"
+	"runtime"
+	"time"
+)
+
+var (
+	confFile string // 配置文件路径
+)
+
+// 解析命令行参数
+func initArgs() {
+	// worker -config ./worker.json
+	// worker -h
+	flag.StringVar(&confFile, "config", "./worker.json", "worker.json")
+	flag.Parse()
+}
+
+// 初始化线程数量
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	var (
+		err error
+	)
+
+	// 初始化命令行参数
+	initArgs()
+
+	// 初始化线程
+	initEnv()
+
+	if err = worker.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	// 正常退出
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+	return
+
+ERR:
+	fmt.Println(err)
+}
