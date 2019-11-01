1428a9b2b9a7cded13d3e8204b931baf3eebe017
norman-chen <nochen@xogrp.com>
2019-11-01 16:59:29 +0800

    worker jobMgr

--- crontab/common/Protocal.go
+++ crontab/common/Protocal.go
@@ -1,6 +1,13 @@
 package common
 
-import "encoding/json"
+import (
+	"context"
+	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/gorhill/cronexpr"
+)
 
 type Job struct {
 	Name     string `json:name`
@@ -13,6 +20,12 @@ type Response struct {
 	Errno int         `json:"errno"`
 	Msg   string      `json:"msg"`
 	Data  interface{} `json:"data"`
+}
+
+// 变化事件
+type JobEvent struct {
+	EventType int //  SAVE, DELETE
+	Job       *Job
 }
 
 // 应答方法
@@ -29,4 +42,73 @@ func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err er
 	// 2, 序列化json
 	resp, err = json.Marshal(response)
 	return
+}
+
+// 反序列化Job
+func UnpackJob(value []byte) (ret *Job, err error) {
+	var (
+		job *Job
+	)
+
+	job = &Job{}
+	if err = json.Unmarshal(value, job); err != nil {
+		return
+	}
+	ret = job
+	return
+}
+
+// 从etcd的key中提取任务名
+// /cron/jobs/job10抹掉/cron/jobs/
+func ExtractJobName(jobKey string) string {
+	return strings.TrimPrefix(jobKey, JOB_SAVE_DIR)
+}
+
+// 从 /cron/killer/job10提取job10
+func ExtractKillerName(killerKey string) string {
+	return strings.TrimPrefix(killerKey, JOB_KILLER_DIR)
+}
+
+// 任务变化事件有2种：1）更新任务 2）删除任务
+func BuildJobEvent(eventType int, job *Job) (jobEvent *JobEvent) {
+	return &JobEvent{
+		EventType: eventType,
+		Job:       job,
+	}
+}
+
+// 构造任务执行计划
+func BuildJobSchedulePlan(job *Job) (jobSchedulePlan *JobSchedulePlan, err error) {
+	var (
+		expr *cronexpr.Expression
+	)
+
+	// 解析JOB的cron表达式
+	if expr, err = cronexpr.Parse(job.CronExpr); err != nil {
+		return
+	}
+
+	// 生成任务调度计划对象
+	jobSchedulePlan = &JobSchedulePlan{
+		Job:      job,
+		Expr:     expr,
+		NextTime: expr.Next(time.Now()),
+	}
+	return
+}
+
+// 构造执行状态信息
+func BuildJobExecuteInfo(jobSchedulePlan *JobSchedulePlan) (jobExecuteInfo *JobExecuteInfo) {
+	jobExecuteInfo = &JobExecuteInfo{
+		Job:      jobSchedulePlan.Job,
+		PlanTime: jobSchedulePlan.NextTime, // 计算调度时间
+		RealTime: time.Now(),               // 真实调度时间
+	}
+	jobExecuteInfo.CancelCtx, jobExecuteInfo.CancelFunc = context.WithCancel(context.TODO())
+	return
+}
+
+// 提取worker的IP
+func ExtractWorkerIP(regKey string) string {
+	return strings.TrimPrefix(regKey, JOB_WORKER_DIR)
 }
---
+++ crontab/worker/JobMgr.go
@@ -0,0 +1,142 @@
+package worker
+
+import (
+	"context"
+	"go-crontab/crontab/common"
+	"time"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+	"go.etcd.io/etcd/clientv3"
+)
+
+type JobMgr struct {
+	client  *clientv3.Client
+	kv      clientv3.KV
+	lease   clientv3.Lease
+	watcher clientv3.Watcher
+}
+
+var G_jobMgr *JobMgr
+
+func InitJobMgr() (err error) {
+	var (
+		config  clientv3.Config
+		client  *clientv3.Client
+		kv      clientv3.KV
+		lease   clientv3.Lease
+		watcher clientv3.Watcher
+	)
+
+	config = clientv3.Config{
+		Endpoints:   G_config.EtcdEndpoints,
+		DialTimeout: time.Duration(G_config.EtcdDialTimeout),
+	}
+
+	if client, err = clientv3.New(config); err != nil {
+		return
+	}
+
+	kv = clientv3.NewKV(client)
+	lease = clientv3.NewLease(client)
+	watcher = clientv3.NewWatcher(client)
+
+	G_jobMgr = &JobMgr{
+		client: client,
+		kv: kv,
+		lease: lease,
+		watcher: watcher
+	}
+
+	G_jobMgr.watchJobs()
+
+	G_jobMgr.watchKiller()
+
+	return
+
+}
+
+func (jobMgr *JobMgr) watchJobs() (err error) {
+	var (
+		getResp            *clientv3.GetResponse
+		kvpair             *mvccpb.KeyValue
+		job                *common.Job
+		watchStartRevision int64
+		watchChan          clientv3.WatchChan
+		watchResp          clientv3.WatchResponse
+		watchEvent         *clientv3.Event
+		jobName            string
+		jobEvent           *commom.JobEvent
+	)
+
+	if getResp, err = jobMgr.kv.Get(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithPrevKV()); err != nil {
+		return
+	}
+
+	for _, kvpair = range getResp.Kvs {
+		if job, err = common.UnpackJob(kvpair.Value); err == nil {
+			jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE, job)
+
+			// G_scheduler.PushJobEvent(jobEvent)
+		}
+	}
+
+	go func() {
+		watchStartRevision = getResp.Header.Revision + 1
+
+		watchStartRevision = jobMgr.watcher.Watch(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithRev(watchStartRevision), clientv3.WithPrefix())
+
+		for watchResp = range watchChan {
+			for _, watchEvent = range watchResp.Events {
+				switch watchEvent.Type {
+				case mvccpb.PUT:
+					if job, err = common.UnpackJob(watchEvent.Kv.Value); err != nil {
+						continue
+					}
+
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE, job)
+
+				case mvccpb.DELETE:
+					jobName = common.ExtractJobName(string(watchEvent.Kv.Key))
+
+					job = &common.Job{Name: jobName}
+
+					// 构建一个删除Event
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_DELETE, job)
+
+				}
+
+				// G_scheduler.PushJobEvent(jobEvent)
+			}
+
+		}
+	}()
+}
+
+func (jobMgr *JobMgr) watchKiller() {
+	var (
+		watchChan clientv3.WatchChan
+		watchResp clientv3.WatchResponse
+		watchEvent *clientv3.Event
+		jobEvent *common.JobEvent
+		jobName string
+		job *common.Job
+	)
+	// 监听/cron/killer目录
+	go func() { // 监听协程
+		// 监听/cron/killer/目录的变化
+		watchChan = jobMgr.watcher.Watch(context.TODO(), common.JOB_KILLER_DIR, clientv3.WithPrefix())
+		// 处理监听事件
+		for watchResp = range watchChan {
+			for _, watchEvent = range watchResp.Events {
+				switch watchEvent.Type {
+				case mvccpb.PUT: // 杀死任务事件
+					jobName = common.ExtractKillerName(string(watchEvent.Kv.Key))
+					job = &common.Job{Name: jobName}
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_KILL, job)
+					// 事件推给scheduler
+					// G_scheduler.PushJobEvent(jobEvent)
+				case mvccpb.DELETE: // killer标记过期, 被自动删除
+				}
+			}
+		}
+	}()
+}
